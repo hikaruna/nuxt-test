5157466b6d07d392ccfba274f9c21d6bdfd394d2
hikaruna <me@hikaruna.net>
2019-01-06 01:31:53 +0900

    もう何もかも消した

--- .nuxt.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  modules: ["nuxt-typescript"]
-}
--- pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>
-    <h1>{{ message }}</h1>
-    <button @click="increment">{{ counter }}</button>
-  </section>
-</template>
-
-<style src="./style.css" scoped></style>
-<script src="./script.ts"></script>
