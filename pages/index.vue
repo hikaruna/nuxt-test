7ede684d98e3fa3de930b49f87eb846731646da4
hikaruna <me@hikaruna.net>
2019-01-06 05:38:47 +0900

    うご！？うごいた！？やったーtypescriptのclass-componentがうごいたああ

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>hello</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+
+}
+</script>
